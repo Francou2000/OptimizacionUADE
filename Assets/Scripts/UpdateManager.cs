9f0dba1600c118ab3ff362a02e7d16df4cab2756
SiragusWolf <76983197+SiragusWolf@users.noreply.github.com>
2024-11-22 09:59:27 -0300

    CustomUpdate applied and improved
    Added cloneI to prevent lambdas (wtf is a lambda)
    Script execution order customized

--- Assets/Scripts/UpdateManager.cs
+++ Assets/Scripts/UpdateManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class UpdateManager : MonoBehaviour
 {
     [SerializeField] public List<IUpdateable> updateables = new List<IUpdateable>();
-    [SerializeField] int a;
     public bool isplaying;
 
     void Start()
@@ -13,21 +12,21 @@ public class UpdateManager : MonoBehaviour
         isplaying = true;
     }
 
-
     void Update()
     {
         if (!isplaying) return;
 
         for (int i = updateables.Count - 1; i >= 0; i--)
         {
-            var updateable = updateables[i];
+            int cloneI = i;
+            var updateable = updateables[cloneI];
             if (updateable != null && updateable.isActiveAndEnabled)
             {
                 updateable.CustomUpdate();
             }
             else
             {
-                updateables.RemoveAt(i);
+                updateables.RemoveAt(cloneI);
             }
         }
     }
